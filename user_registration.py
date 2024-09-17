9f94f3fab890c76725bb2550413255c9043eb4d6
unknown <keshav.wanjale@gmail.com>
2024-09-18 00:38:47 +0530

    [Keshav] Add: Python script for verifying last name.

--- user_registration.py
+++ user_registration.py
@@ -19,6 +19,27 @@ def validate_first_name(first_name):
         print(f"First name: {first_name} is invalid.",
               "\nName must start with Capital and atleast have 3 characters.")
 
+def validate_last_name(last_name):
+    """
+    Description:
+        Validates the last name based on specific rules:
+        - The last name must start with a capital letter.
+        - The last name must have a minimum of 3 characters.
+    Parameters:
+        last_name (str): The last name to be validated.
+    Return:
+        bool: Returns True if the last name is valid, otherwise False.
+    """
+    pattern = re.compile(r'^[A-Z][a-z]{2,}$')
+
+    if pattern.match(last_name) :
+        print(f"Last name: {last_name} is valid.")
+    else :
+        print(f"Last name: {last_name} is invalid.",
+              "\nName must start with Capital and atleast have 3 characters.")
+
 
 first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 validate_first_name(first_name)
+validate_last_name(last_name)
